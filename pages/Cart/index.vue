f837bd301f69ee08663b33b9e710b8c66800f2f2
Praveen Malethia <prince.malethia.36@gmail.com>
2021-12-12 01:18:07 +0530

    Cart Page

--- pages/Cart/index.vue
+++ pages/Cart/index.vue
@@ -1,8 +1,49 @@
 <template>
-  <div class="px-10">
+  <div class="px-2">
     <v-row class="mt-2">
       <v-col cols="12" sm="12" md="8">
-        <v-card class="logo py-10 d-flex justify-center"> </v-card>
+        <v-toolbar dense rounded elevation="0">
+          <v-toolbar-title>Cart</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-icon class="mr-2"> mdi-truck-outline</v-icon>
+          <span class="mr-4">Deliver to</span>
+          <v-select
+            v-model="e2"
+            :items="states"
+            menu-props="auto"
+            hide-details
+            label="Select"
+            single-line
+          ></v-select>
+        </v-toolbar>
+        <v-list three-line>
+          <template v-for="(item, index) in items">
+            <v-subheader
+              v-if="item.header"
+              :key="item.header"
+              v-text="item.header"
+            ></v-subheader>
+
+            <v-divider
+              v-else-if="item.divider"
+              :key="index"
+              :inset="item.inset"
+            ></v-divider>
+
+            <v-list-item v-else :key="item.title">
+              <v-list-item-avatar>
+                <v-img :src="item.avatar"></v-img>
+              </v-list-item-avatar>
+
+              <v-list-item-content>
+                <v-list-item-title v-html="item.title"></v-list-item-title>
+                <v-list-item-subtitle
+                  v-html="item.subtitle"
+                ></v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </template>
+        </v-list>
       </v-col>
       <v-col cols="12" sm="12" md="4">
         <v-card class="logo py-10 d-flex justify-center"> </v-card>
@@ -13,7 +54,44 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    e2: 'Texas',
+    states: ['Churiwala Dhanna', 'Luxury PG'],
+    items: [
+        { header: 'Today' },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+          title: 'Brunch this weekend?',
+          subtitle: `<span class="text--primary">Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle: `<span class="text--primary">to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend.`,
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+          title: 'Oui oui',
+          subtitle: '<span class="text--primary">Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+          title: 'Birthday gift',
+          subtitle: '<span class="text--primary">Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?',
+        },
+        { divider: true, inset: true },
+        {
+          avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+          title: 'Recipe to try',
+          subtitle: '<span class="text--primary">Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos.',
+        },
+      ],
+  }),
+}
 </script>
 
 <style></style>
