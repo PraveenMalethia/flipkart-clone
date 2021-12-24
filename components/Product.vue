2e4a900d43a244d832a99aec7f2206d053cb131d
Praveen Malethia <prince.malethia.36@gmail.com>
2021-12-24 20:03:43 +0530

    Product page making

--- components/Product.vue
+++ components/Product.vue
@@ -1,53 +1,55 @@
 <template>
   <v-hover v-slot="{ hover }" class="mt-2 ma-1">
-    <v-card
-      class="ma-1"
-      color="grey lighten-4"
-      elevation="0"
-      height="250"
-      width="200"
-    >
-      <v-img :aspect-ratio="16 / 14" :src="product_image">
-        <v-expand-transition>
-          <div
-            v-if="hover"
-            class="d-flex transition-fast-in-fast-out black darken-3 v-card--reveal text-h6 white--text"
-            style="height: 100%"
+    <nuxt-link to="/Product/1">
+      <v-card
+        class="ma-1"
+        color="grey lighten-4"
+        elevation="0"
+        height="250"
+        width="200"
+      >
+        <v-img :aspect-ratio="16 / 14" :src="product_image">
+          <v-expand-transition>
+            <div
+              v-if="hover"
+              class="d-flex transition-fast-in-fast-out black darken-3 v-card--reveal text-h6 white--text"
+              style="height: 100%"
+            >
+              $14.99
+            </div>
+          </v-expand-transition>
+        </v-img>
+        <v-card-text style="position: relative">
+          <v-btn
+            absolute
+            color="blue darken-2"
+            class="white--text"
+            fab
+            @click="AddToCart"
+            right
+            top
+            small
+            depressed
+            :loading="loading"
           >
-            $14.99
+            <v-icon>mdi-cart-outline</v-icon>
+          </v-btn>
+          <div class="font-weight-light black--text font-bold mb-2">
+            {{ product_image == 'feature4.jpg' ? 'Iphone' : 'Designer Shoes' }}
           </div>
-        </v-expand-transition>
-      </v-img>
-      <v-card-text style="position: relative">
-        <v-btn
-          absolute
-          color="blue darken-2"
-          class="white--text"
-          fab
-          @click="AddToCart"
-          right
-          top
-          small
-          depressed
-          :loading="loading"
-        >
-          <v-icon>mdi-cart-outline</v-icon>
-        </v-btn>
-        <div class="font-weight-light black--text font-bold mb-2">
-          {{ product_image == 'feature4.jpg' ? 'Iphone' :'Designer Shoes'}}
-        </div>
-        <p class="font-weight-bold green--text mb-2">30% off</p>
-      </v-card-text>
-    </v-card>
+          <p class="font-weight-bold green--text mb-2">30% off</p>
+        </v-card-text>
+      </v-card>
+    </nuxt-link>
   </v-hover>
 </template>
 
 <script>
 export default {
-  props:{
-    product_image:{
-      type:String,
-      default:''
+  props: {
+    product_image: {
+      type: String,
+      default: '',
     },
   },
   data: () => ({
---
+++ pages/Product/_id.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="container mt-10">
+    <v-row>
+      <v-col cols="12" sm="12" md="5">
+        <v-card elevation="0">
+          <template>
+            <v-carousel hide-delimiter-background :show-arrows="false">
+              <v-carousel-item
+                v-for="(item, i) in items"
+                :key="i"
+                :src="item.src"
+              ></v-carousel-item>
+            </v-carousel>
+          </template>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="12" md="7">
+        <v-breadcrumbs :items="breadCrumbs">
+          <template v-slot:divider>
+            <v-icon>mdi-chevron-right</v-icon>
+          </template>
+        </v-breadcrumbs>
+        <div class="ms-7">
+          <h2 class="black--text font-weight-regular">
+            Apple iPhone 12 (Pacific Blue , 256 GB)
+          </h2>
+          <v-btn small elevation="0" class="black--text">
+            4 <v-icon color="yellow darken-2">mdi-star</v-icon>
+          </v-btn>
+          <span>1,036 Ratings & 71 Reviews</span>
+          <h1 class="mt-4">₹ 1,19,900</h1>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      breadCrumbs: [
+        {
+          text: 'Home',
+          disabled: false,
+          href: '/',
+        },
+        {
+          text: 'SmaprtPhones',
+          disabled: false,
+          href: '/Smartphones',
+        },
+        {
+          text: 'Apple Iphone 12',
+          disabled: true,
+          href: '/Product2',
+        },
+      ],
+      items: [
+        {
+          src: '/iphone1.jfif',
+        },
+        {
+          src: '/iphone1.jfif',
+        },
+        {
+          src: '/iphone1.jfif',
+        },
+        {
+          src: '/iphone1.jfif',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style></style>
